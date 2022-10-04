a4cd7f6ff33854ea9e7d204aaf98b5223680403f
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2022-10-04 14:24:03 -0400

    fix(useAuth): ens check

--- apps/site/src/hooks/useAuth.ts
+++ apps/site/src/hooks/useAuth.ts
@@ -13,25 +13,28 @@ import {
 
 export function useAuth() {
   const provider = useProvider()
-
+  const { chain } = useNetwork()
+  const { disconnect } = useDisconnect()
   const { data: signer } = useSigner()
+
   const { address, isConnecting, isConnected } = useAccount()
   let { data: ensName } = useEnsName({
     address: address,
+    chainId: 1
   })
+
   const { data: ensAvatar } = useEnsAvatar({
     addressOrName: address,
+    chainId: 1
   })
-  const { disconnect } = useDisconnect()
-  const { chain } = useNetwork()
   const { data: balance } = useBalance({ addressOrName: address })
 
   return {
     provider,
     signer,
     address: address,
-    ensName: ensName || truncateEthAddress(address),
-    ensAvatar: ensAvatar,
+    ensName: ensName ?? truncateEthAddress(address),
+    ensAvatar: ensAvatar ?? null,
     displayName: ensName ?? truncateEthAddress(address),
     balance: balance,
     loading: isConnecting,
